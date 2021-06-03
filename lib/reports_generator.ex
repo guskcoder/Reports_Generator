781cc20c7be090a699041718e5c02783dbe9ec47
guskcoder <gustavo@imaginative.company>
2021-06-03 10:14:41 -0300

    Reports Add

---
+++ lib/reports_generator.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGenerator do
+  @moduledoc """
+  Documentation for `ReportsGeneratorRocketseat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsGeneratorRocketseat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
