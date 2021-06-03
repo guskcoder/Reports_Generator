a9ec94174913e1bf31880cc568855d7df5257b63
guskcoder <gustavo@imaginative.company>
2021-06-03 10:46:26 -0300

    Inicial Function

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,8 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGeneratorRocketseat`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGeneratorRocketseat.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(filename) do
+    case File.read("reports/#{filename}") do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
   end
 end
